eb8014d293c7c1e7ea11931db4d0d84ff0761e56
Varun Tulsiyani <90818402+Varun-Tulsiyani@users.noreply.github.com>
2023-07-26 14:18:59 +0400

    Stage 2

--- Java/Cinema Room Manager/src/cinema/Cinema.java
+++ Java/Cinema Room Manager/src/cinema/Cinema.java
@@ -1,8 +1,13 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
     public static void main(String[] args) {
         // Write your code here
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("-----STAGE ONE-----");
         System.out.println("Cinema:");
         for (int i = 0; i <= 7; i++) {
             if (i == 0) {
@@ -21,5 +26,31 @@ public class Cinema {
                 }
             }
         }
+
+        System.out.println("\n-----STAGE ONE-----");
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row");
+        int seats = scanner.nextInt();
+
+        int totalSeats = rows * seats;
+        int profit = 0, frontHalf = 0, backHalf = 0;
+
+        if (rows % 2 != 0) {
+            frontHalf = ((int) Math.floor(rows / 2.00)) * seats;
+            backHalf = ((int) Math.ceil(rows / 2.00)) * seats;
+        } else {
+            frontHalf = totalSeats / 2;
+            backHalf = totalSeats / 2;
+        }
+
+        if (totalSeats <= 60) {
+            profit = totalSeats * 10;
+        } else {
+            profit = (frontHalf * 10) + (backHalf * 8);
+        }
+
+        System.out.println("Total income:\n$" + profit);
+        scanner.close();
     }
 }
